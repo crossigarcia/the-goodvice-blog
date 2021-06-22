7819b709acf1d852cc41ec9af7c2259889a7af37
crosgar <cecilia.rossi.garcia@gmail.com>
2021-06-21 21:05:56 -0700

    updated index.js for models, updated seeds

--- models/index.js
+++ models/index.js
@@ -5,47 +5,47 @@ const Post = require("./Post");
 const Tag = require("./Tag");
 const PostTag = require('./PostTag');
 
-//user has many post
-User.hasMany(Post, {
-  foreignKey: "user_id",
-});
+// //user has many post
+// User.hasMany(Post, {
+//   foreignKey: "post_id",
+// });
 
-//user belongs to many comment
-User.hasMany(Comment, {
-   foreignKey: "user_id"
-});
+// //user belongs to many comment
+// User.hasMany(Comment, {
+//    foreignKey: "comment_id"
+// });
 
-//comment belongs to user
-Comment.belongsTo(User, {
-   foreignKey: 'user_id'
-});
+// //comment belongs to user
+// Comment.belongsTo(User, {
+//    foreignKey: 'comment_id'
+// });
 
-Post.hasMany(Comment, {
-   foreignKey: "user_id"
-});
+// Post.hasMany(Comment, {
+//    foreignKey: "user_id"
+// });
 
-//post belongs to user
-Post.belongsTo(User, {
-   foreignKey: "post_id"
-});
+// //post belongs to user
+// Post.belongsTo(User, {
+//    foreignKey: "user_id"
+// });
 
-//post has many tags
-Post.hasMany(Tag, {
-   through: PostTag,
-   foreignKey: 'post_id'
+// //post has many tags
+// Post.belongsToMany(Tag, {
+//    through: PostTag,
+//    foreignKey: 'post_id'
 
-});
+// });
 
-// //tags belongs to many posts
-Tag.belongsToMany(Post, {
-   through: PostTag,
-   foreignKey: 'tag_id'
-});
+// // //tags belongs to many posts
+// Tag.belongsToMany(Post, {
+//    through: PostTag,
+//    foreignKey: 'tag_id'
+// });
 
-//comment belongs to post
-Comment.belongsTo(Post, {
-   foreignKey: 'post_id'
-});
+// //comment belongs to post
+// Comment.belongsTo(Post, {
+//    foreignKey: 'post_id'
+// });
 
 
 module.exports = { User, Post, Comment, Tag, PostTag };
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,7 @@
 const seedUsers = require('./user-seeds');
+const seedComments = require('./comment-seeds');
+const seedPosts = require('./post-seeds');
+const seedTags = require('./tag-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -6,6 +9,12 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
   await seedUsers();
+
+  await seedComments();
+
+  await seedPosts();
+
+  await seedTags();
 
   process.exit(0);
 };
