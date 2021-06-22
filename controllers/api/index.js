8a1ccbbf6144629628a3946db37f293501817b3b
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-21 17:13:19 -0700

    added comment-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 
