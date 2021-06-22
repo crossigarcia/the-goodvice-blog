6a065f042b8d421054a673702dfafbcbb52e1d87
nicopetelo <nicopetelo@gmail.com>
2021-06-21 18:47:42 -0700

    started tags

--- models/Tag.js
+++ models/Tag.js
@@ -2,10 +2,10 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
+class Tag extends Model {}
 
 // create fields/columns for Post model
-Post.init(
+Tag.init(
     {
       id: {
         type: DataTypes.INTEGER,
@@ -13,16 +13,12 @@ Post.init(
         primaryKey: true,
         autoIncrement: true
       },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      post_text: {
+      tag_text: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
             // this means the post_text must be at least four characters long
-            len: [4]
+            len: [2]
           }
       },
       user_id: {
@@ -32,10 +28,10 @@ Post.init(
           key: 'id'
         }
       },
-      tag_id : {
+      post_id: {
         type: DataTypes.INTEGER,
         references: {
-          model: 'tag',
+          model: 'post',
           key: 'id'
         }
       }
@@ -44,8 +40,8 @@ Post.init(
       sequelize,
       freezeTableName: true,
       underscored: true,
-      modelName: 'post'
+      modelName: 'tag'
     }
   );
 
-  module.exports = Post;
+  module.exports = Tag;
