baff0ae6e4060397c58f6089a2963fda4ad5d1c7
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-21 17:23:15 -0700

    applied router.use to api/index.js for comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,5 +4,6 @@ const userRoutes = require('./user-routes.js');
 const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
