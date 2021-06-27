df826076ac0e3b5e936e44d83ba5890ffc68c2ea
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-27 11:27:00 -0700

    routes are tested and working

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
+const { endsWith } = require("sequelize/types/lib/operators");
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
+  console.log("==========================");
   Comment.findAll()
     .then((dbCommentData) => res.json(dbCommentData))
     .catch((err) => {
@@ -12,13 +14,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
+  console.log("==========================");
   // check the session
   if (req.session) {
     Comment.create({
       comment_text: req.body.comment_text,
-      post_id: req.session.post_id,
+      post_id: req.body.post_id,
       // use the id from the session
-      user_id: req.session.user_id,
+      user_id: req.body.user_id,
     })
       .then((dbCommentData) => res.json(dbCommentData))
       .catch((err) => {
@@ -29,11 +32,18 @@ router.post("/", withAuth, (req, res) => {
 });
 
 router.put("/:id", withAuth, (req, res) => {
-  Comment.update({
-    where: {
-      id: req.params.id,
+  console.log("==========================");
+  // update a comment by its id value 
+  Comment.update(
+    {
+      comment_text: req.body.comment_text,
     },
-  })
+    {
+      where: {
+      id: req.params.id
+    }
+    }
+  )
     .then((dbCommentData) => {
       if (!dbCommentData) {
         res.status(404).json({ message: "No comment found with this id" });
@@ -48,6 +58,7 @@ router.put("/:id", withAuth, (req, res) => {
 });
 
 router.delete("/:id", withAuth, (req, res) => {
+  console.log("==========================");
   Comment.destroy({
     where: {
       id: req.params.id,
