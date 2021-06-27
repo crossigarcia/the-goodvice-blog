c35252bef8e79578f8556b4fce31231d403672c0
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-06-27 14:56:18 -0700

    added comment routes info

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { endsWith } = require("sequelize/types/lib/operators");
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
