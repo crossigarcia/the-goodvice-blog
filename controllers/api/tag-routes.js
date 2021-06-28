c88e1383628ddbe397a06e04745fdc88447d1a40
nicopetelo <nicopetelo@gmail.com>
2021-06-27 18:16:54 -0700

    working on tag search

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -76,6 +76,66 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+// search by tag
+router.get('/:id', (req, res) => {
+    Post.findAll({
+        where: {
+            tag_id: req.params.id
+        }
+        , attributes: ['post_id']
+    })
+    .then(dbTags => {
+        const postIds = dbTags.map(postTag => postTag.get({plain:true}).post_id);
+        return Post.findAll({
+            where: {
+                id: postIds
+            },
+            attributes: [
+                'id',
+                'title',
+                'post_text',
+                'created_at',
+                'tag_id'
+            ]
+        })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 
+// search by query
+router.get("/q=:query", (req, res) => {
+    let query = req.params.query;
+    Post.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+          {
+            post_text: {
+              [Op.like]: `%${query}%`,
+            },
+          },
+        ],
+      },
+      attributes: ["title", "post_text"],
+    })
+      .then((dbTagData) => {
+        if (!dbTagData) {
+          res.status(404).json({ message: "No tag found with this id" });
+          return;
+        }
+        res.json(dbTagData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -1,38 +1,39 @@
 const router = require("express").Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment,Tag, PostTag } = require('../models');
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Tag, PostTag } = require("../models");
 const { Op } = require("sequelize");
 
-
-// router.get("/search?:query", (req, res) => {
-  router.get("/query=:query", (req, res) => {
-    let query = req.params.query;
-    Post.findAll({
-      where: {
-        [Op.or]: [
-            { title: {
-                    [Op.like]: `%${query}%`
-                }
-            },
-            { post_text: {
-                    [Op.like]: `%${query}%`
-                }
-            }
-        ] 
+// search by query
+router.get("/q=:query", (req, res) => {
+  let query = req.params.query;
+  Post.findAll({
+    where: {
+      [Op.or]: [
+        {
+          title: {
+            [Op.like]: `%${query}%`,
+          },
+        },
+        {
+          post_text: {
+            [Op.like]: `%${query}%`,
+          },
+        },
+      ],
+    },
+    attributes: ["title", "post_text"],
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
       }
-      , attributes: ["title", "post_text"]
+      res.json(dbTagData);
     })
-      .then((dbTagData) => {
-        if (!dbTagData) {
-          res.status(404).json({ message: "No tag found with this id" });
-          return;
-        }
-        res.json(dbTagData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-  module.exports = router;
+module.exports = router;
