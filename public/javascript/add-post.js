9777f59bbee47c20ed198acc2f7d017ab26ea90d
crosgar <cecilia.rossi.garcia@gmail.com>
2021-06-29 15:28:52 -0700

    styling dashboard cards

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,3 +1,38 @@
+
+// Get the modal
+var modal = document.getElementById("modalOne");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("button");
+
+var closeBtn = document.getElementById("cancel");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+modal.style.display = "none";
+}
+
+closeBtn.onclick = function () {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
+
 async function newFormHandler(event) {
     event.preventDefault();
   
@@ -36,13 +71,13 @@ async function newFormHandler(event) {
   }
   
   
-  function openTheForm() {
-    document.getElementById("popupForm").style.display = "block";
-  }
-  
-  function closeTheForm() {
-    document.getElementById("popupForm").style.display = "none";
-  }
-  
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+function openTheForm() {
+document.getElementById("popupForm").style.display = "block";
+}
+
+function closeTheForm() {
+document.getElementById("popupForm").style.display = "none";
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
   
