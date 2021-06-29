6972ee67ead8d2f04857a94e433487d8b2cd8ce6
crosgar <cecilia.rossi.garcia@gmail.com>
2021-06-29 13:58:18 -0700

    reverted to older commits, added new css

--- seeds/post-tag-seeds.js
+++ seeds/post-tag-seeds.js
@@ -4,24 +4,7 @@ const postTagData = [
     {
         post_id: 1,
         tag_id: 5
-    },
-    {
-
-        post_id: 2,
-        tag_id: 5,
-    },
-    {
-        post_id: 3,
-        tag_id: 3,
-    },
-    {
-        post_id: 4,
-        tag_id: 2,
-    },
-    {
-        post_id: 5,
-        tag_id: 1,
-    },
+    }
 ];
 
 const seedPostTags = () => PostTag.bulkCreate(postTagData);
